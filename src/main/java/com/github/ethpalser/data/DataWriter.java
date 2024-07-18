754ca82688b65a8250f9bec6975c7f74976f5fb5
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2024-07-17 21:12:21 -0400

    Create DataWriter.java
    Created DataWriter to wrap around JsonWriter to write objects to file as JSON

---
+++ src/main/java/com/github/ethpalser/data/DataWriter.java
@@ -0,0 +1,34 @@
+package com.github.ethpalser.data;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.stream.JsonWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+
+public class DataWriter {
+
+    public DataWriter() {
+        // empty
+    }
+
+    public void write(Object object, Type type, String filePath) {
+        Gson gson = new GsonBuilder().serializeNulls().create();
+        try (FileWriter writer = new FileWriter(filePath)) {
+            JsonWriter jsonWriter = new JsonWriter(writer);
+            gson.toJson(object, type, jsonWriter);
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+
+    public void writeSaveFile(SaveData saveData, String fileName) {
+        this.write(saveData, SaveData.class, SaveData.FILE_DIR + fileName);
+    }
+
+    public void writePieceFile(PieceData pieceData, String fileName) {
+        this.write(pieceData, PieceData.class, PieceData.FILE_DIR + fileName);
+    }
+
+}
